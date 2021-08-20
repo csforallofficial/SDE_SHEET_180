e23daa5fde1b44986c1934cd25b95ee6fbde3bc1
Sunyul Hossen <60459287+xenonreborn@users.noreply.github.com>
2021-08-20 23:35:31 +0530

    Create rotate_linked_list_by_k.cpp

---
+++ rotate_linked_list_by_k.cpp
@@ -0,0 +1,57 @@
+/*
+Given the head of a linked list, rotate the list to the right by k places.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [4,5,1,2,3]
+Example 2:
+
+
+Input: head = [0,1,2], k = 4
+Output: [2,0,1]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 500].
+-100 <= Node.val <= 100
+0 <= k <= 2 * 109
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head==NULL || head->next==NULL || k==0)
+            return head;
+        ListNode* cur = head;
+        int len = 1;
+        while(cur->next!=NULL){
+            cur=cur->next;
+            len++;
+        }
+        cur->next = head;
+        k = k%len;
+        k = len-k;
+        while(k--){
+            cur = cur->next;
+        }
+        head = cur->next;
+        cur->next = NULL;
+        return head;
+    }
+};
