e52cae6d9b3bddb1598e2c6158ac1c551754b71e
Sunyul Hossen <60459287+xenonreborn@users.noreply.github.com>
2021-09-17 12:39:55 +0530

    Update search_in_2d_matrix.cpp

--- search_in_2d_matrix.cpp
+++ search_in_2d_matrix.cpp
@@ -24,13 +24,34 @@ n == matrix[i].length
 -104 <= matrix[i][j], target <= 104 */
 
 
+//1st approach - start from the top right and traverse accordingly
+//Time - O(N^2)
+//Space - O(1)
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int i = 0, j = m-1;
+        while(i < n && j >= 0){
+            if(matrix[i][j] == target)
+                return true;
+            else if(matrix[i][j] < target)
+                i++;
+            else
+                j--;
+        }
+        return false;
+    }
+};
 
 
+--------------------------------------------------------------------------------------------------------------------------------------------
 
-
-
-
-
+//2nd approach - Using modules
+//Time - O(N)
+//Space - O(1)
 
 class Solution {
 public:
