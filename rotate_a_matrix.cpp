73c900d2b095f6cc3c2758a8dcf585a996ed6f8f
Sunyul Hossen <60459287+xenonreborn@users.noreply.github.com>
2021-09-17 11:57:56 +0530

    Update rotate_a_matrix.cpp

--- rotate_a_matrix.cpp
+++ rotate_a_matrix.cpp
@@ -1,3 +1,35 @@
+//1st approach - Normally assign a new array and fill it accordingly
+//Time - O(N^2)
+//Space - O(N^2)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int ro,co;
+    cin >> ro >> co;
+    int arr[ro][co];
+    for(int i = 0; i < ro; i++){
+        for(int j = 0; j < co; j++)
+            cin >> arr[i][j];
+    }
+    int ans[co][ro];
+    for(int i = 0; i < co; i++){
+        for(int j = 0; j < ro; j++){
+            ans[i][j] = arr[ro-j-1][i];
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//2nd approach - Transpose the array and reverse every row
+//Time - O(N^2) 
+//Space - O(1)
+
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
