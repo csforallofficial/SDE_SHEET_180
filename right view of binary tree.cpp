ff217fa94607cb2fdbe35d28149e4ea0c3834235
Sunyul Hossen <60459287+xenonreborn@users.noreply.github.com>
2021-10-05 14:27:30 +0530

    Create right view of binary tree.cpp

---
+++ right view of binary tree.cpp
@@ -0,0 +1,55 @@
+/*
+Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,null,5,null,4]
+Output: [1,3,4]
+Example 2:
+
+Input: root = [1,null,3]
+Output: [1,3]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        rightview(root,ans,0);
+        return ans;
+    }
+    void rightview(TreeNode* root, vector<int>& ans, int level){
+        if(root == NULL)
+            return;
+        if(ans.size() == level)
+            ans.push_back(root->val);
+        rightview(root->right,ans,level+1);
+        rightview(root->left,ans,level+1);
+    }
+    
+};
