0b94a4262c21ac309881f5148c941c731a16247b
Sunyul Hossen <60459287+xenonreborn@users.noreply.github.com>
2021-10-06 20:13:44 +0530

    Create check if binary tree is height balanced or not.cpp

---
+++ check if binary tree is height balanced or not.cpp
@@ -0,0 +1,62 @@
+/*
+Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as:
+
+a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: true
+Example 2:
+
+
+Input: root = [1,2,2,3,3,null,null,4,4]
+Output: false
+Example 3:
+
+Input: root = []
+Output: true
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-104 <= Node.val <= 104
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return height(root) != -1;
+    }
+    int height(TreeNode* root){
+        if(root == NULL)
+            return 0;
+        int lh = height(root->left);
+        if(lh == -1)
+            return -1;
+        int rh = height(root->right);
+        if(rh == -1)
+            return -1;
+        if(abs(lh-rh)>1)
+            return -1;
+        return 1+max(lh,rh);
+    }
+};
